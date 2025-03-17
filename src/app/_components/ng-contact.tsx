05f793d87cac22c33401c5cc4dabed19518aa76c
even44 <even.andreas.ingeberg@ebnett.no>
2025-03-18 00:29:15 +0100

    made project dedicated to just the norgroup page

---
+++ src/app/_components/ng-contact.tsx
@@ -0,0 +1,13 @@
+export default function NgContact() {
+	return (
+		<div className="flex-col max-w-2xl w-10/12 bg-gray-800 shadow-black shadow-md p-8 rounded-md min-h-50">
+			<h1 className="text-2xl sm:text-3xl md:text-6xl mb-2">Norgruppen AS</h1>
+			<ul>
+				<li><p className="text-xl">Adresse: .....</p></li>
+				<li><p className="text-xl">Telefon: .....</p></li>
+				<li><p className="text-xl">Epost: .....</p></li>
+				<li><p className="text-xl">Foretaksnr. .....</p></li>
+			</ul>
+		</div>
+	)
+}
---
+++ src/app/_components/ng-description.tsx
@@ -0,0 +1,23 @@
+
+export default function NgDescription() {
+	return (
+		<div className="flex-col max-w-2xl w-10/12 bg-gray-800 shadow-black shadow-md p-8 rounded-md min-h-50">
+			<h1 className="text-2xl sm:text-3xl md:text-6xl mb-2">Hva er Norgruppen?</h1>
+			<ul>
+				<p>
+				En lang og fin beskrivelse av norgruppen og hva det er
+				En lang og fin beskrivelse av norgruppen og hva det er
+				En lang og fin beskrivelse av norgruppen og hva det er
+				En lang og fin beskrivelse av norgruppen og hva det er
+				En lang og fin beskrivelse av norgruppen og hva det er
+				En lang og fin beskrivelse av norgruppen og hva det er
+				En lang og fin beskrivelse av norgruppen og hva det er
+				En lang og fin beskrivelse av norgruppen og hva det er
+				En lang og fin beskrivelse av norgruppen og hva det er
+				En lang og fin beskrivelse av norgruppen og hva det er
+				En lang og fin beskrivelse av norgruppen og hva det er
+				</p>
+			</ul>
+		</div>
+	)
+}
